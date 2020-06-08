404e0a56d40b1fd108311c1ce7197088d536f476
Ukor Jidechi E <ukorjidechi@gmail.com>
2020-06-09 00:56:46 +0100

    chore: set-up settings

---
+++ test/index.test.js
@@ -0,0 +1,5 @@
+const test = require('firebase-functions-test')({
+  databaseURL: "https://paystack-webhook-project.firebaseio.com",
+  storageBucket: "paystack-webhook-project.appspot.com",
+  projectId: "paystack-webhook-project",
+}, '../test-cloud-258123-8b535e4067bd.json');
